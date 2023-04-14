fa6dc29f16a147d9b1d32abb6b74e85f3e1b8a9b
dudhfysibw1027 <gendyf87k@gmail.com>
2023-04-14 22:19:19 +0800

    cum_cl add fake_target_parsing

--- utils/utils.py
+++ utils/utils.py
@@ -212,7 +212,7 @@ class image_saver():
         self.path = os.path.join(opt.checkpoints_dir, opt.name, "images") + "/"
         self.opt = opt
         self.num_cl = {"body_seg": opt.semantic_nc[0] + 1, "cloth_seg": opt.semantic_nc[1] + 1,
-                       "densepose_seg": opt.semantic_nc[2] + 1, "fake_parsing": 17, "real_parsing": 17}
+                       "densepose_seg": opt.semantic_nc[2] + 1, "fake_original_parsing": 17, "real_parsing": 17, "fake_target_parsing" : 17}
 
         os.makedirs(self.path, exist_ok=True)
 
