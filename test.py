bfe72a20441c11b5d55a46e477c179da347724b2
dudhfysibw1027 <gendyf87k@gmail.com>
2023-05-20 23:30:06 +0800

    test

--- test.py
+++ test.py
@@ -15,83 +15,6 @@ from dataloaders.VitonDataset import VitonDataset
 from dataloaders.VitonHDDataset import VitonHDDataset
 from utils.plotter import evaluate, plot_simple_reconstructions
 
-def tens_to_im(tens):
-    out = (tens + 1) / 2
-    out.clamp(0, 1)
-    return np.transpose(out.detach().cpu().numpy(), (1, 2, 0))
-def tens_to_lab(tens, num_cl):
-    label_tensor = Colorize(tens, num_cl)
-    label_numpy = np.transpose(label_tensor.numpy(), (1, 2, 0))
-    return label_numpy
-
-#--- read options ---#
-opt = config.read_arguments(train=False)
-
-#--- create dataloader to populate opt ---#
-opt.phase = "test"
-dataloaders.get_dataloaders(opt)
-
-assert opt.phase in {"val", "test"}
-
-if opt.dataset == "mpv":
-    dataset_cl = MPVDataset
-elif opt.dataset == "viton":
-    dataset_cl = VitonDataset
-elif opt.dataset == "vitonHD":
-    dataset_cl = VitonHDDataset
-else:
-    raise NotImplementedError
-
-if (opt.phase == "val" or opt.phase == "test"):
-    model = models.OASIS_model(opt)
-    model = models.put_on_multi_gpus(opt, model)
-    model.eval()
-    
-    image_indices = [2, 7, 8, 18, 35, 36, 38, 45, 47, 52, 56, 57, 58, 60, 63, 64, 66, 72, 74, 80]
-
-    dataset = dataset_cl(opt, phase=opt.phase)
-    evaluate(model, dataset, opt)
-
-if opt.phase == "test":
-    model = models.OASIS_model(opt)
-    model = models.put_on_multi_gpus(opt, model)
-    model.eval()
-
-    dataset = dataset_cl(opt, phase=opt.phase)
-    
-    test_dataloader = DataLoader(dataset, batch_size=1, shuffle=False, drop_last=False)
-    
-    os.makedirs(os.path.join("results", opt.name, opt.phase + "_images"), exist_ok=True)
-    
-    for i, data_i in enumerate(test_dataloader):
-        print(i, "/", len(test_dataloader), end="\r")
-        image, label, human_parsing = models.preprocess_input(opt, data_i)
-        # label["cloth_seg"] = model.module.edit_cloth_seg(image["C_t"], label["body_seg"], label["cloth_seg"])
-        agnostic = data_i["agnostic"] if opt.bpgm_id.find("old") >= 0 else None
-        
-        if opt.no_seg:
-            image["I_m"] = image["I"]
-        
-        pred = model(image, label, "generate", None, agnostic=agnostic).detach().cpu().squeeze().permute(1, 2, 0).numpy()
-        # print(pred.shape)
-        pred = (pred + 1) / 2
-        pred = (pred * 255).astype(np.uint8)
-        pred = cv2.cvtColor(pred, cv2.COLOR_RGB2BGR)
-        # pred = cv2.resize(pred, (data_i['original_size'][1], data_i['original_size'][0]), interpolation=cv2.INTER_LINEAR)
-        pred = cv2.resize(pred, opt.img_size[::-1], interpolation=cv2.INTER_AREA)
-        
-        if opt.dataset == "mpv":
-            filename = data_i['name'][0].split("/")[-1].replace(".jpg", ".png")
-        elif opt.dataset == "viton":
-            filename = data_i['name'][0].split("/")[-1]
-        elif opt.dataset == "vitonHD":
-            filename = data_i['name'][0].split("/")[-1]
-        cv2.imwrite(os.path.join("results", opt.name, opt.phase + "_images", filename), pred)
-        cv2.imwrite(os.path.join("results", opt.name, opt.phase + "_images", filename, "_origin"), tens_to_im(image["I"][0]))
-        cv2.imwrite(os.path.join("results", opt.name, opt.phase + "_images", filename, "_cloth"), tens_to_im(image["C_t"][0]))
-        im = tens_to_lab(label["densepose_seg"][0], opt.semantic_nc[2] + 1)
-        cv2.imwrite(os.path.join("results", opt.name, opt.phase + "_images", filename, "_densepose"), im)
-
 def uint82bin(n, count=8):
     """returns the binary of integer n, count refers to amount of bits"""
     return ''.join([str((n >> y) & 1) for y in range(count - 1, -1, -1)])
@@ -109,7 +32,6 @@ def Colorize(tens, num_cl):
         color_image[1][mask] = cmap[label][1]
         color_image[2][mask] = cmap[label][2]
     return color_image
-
 
 def labelcolormap(N):
     # print('==========N===========')
@@ -192,3 +114,84 @@ def labelcolormap(N):
             cmap[i, 1] = g
             cmap[i, 2] = b
     return cmap
+
+def tens_to_im(tens):
+    out = (tens + 1) / 2
+    out.clamp(0, 1)
+    return np.transpose(out.detach().cpu().numpy(), (1, 2, 0))
+def tens_to_lab(tens, num_cl):
+    label_tensor = Colorize(tens, num_cl)
+    label_numpy = np.transpose(label_tensor.numpy(), (1, 2, 0))
+    return label_numpy
+
+#--- read options ---#
+opt = config.read_arguments(train=False)
+
+#--- create dataloader to populate opt ---#
+opt.phase = "test"
+dataloaders.get_dataloaders(opt)
+
+assert opt.phase in {"val", "test"}
+
+if opt.dataset == "mpv":
+    dataset_cl = MPVDataset
+elif opt.dataset == "viton":
+    dataset_cl = VitonDataset
+elif opt.dataset == "vitonHD":
+    dataset_cl = VitonHDDataset
+else:
+    raise NotImplementedError
+
+if (opt.phase == "val" or opt.phase == "test"):
+    model = models.OASIS_model(opt)
+    model = models.put_on_multi_gpus(opt, model)
+    model.eval()
+    
+    image_indices = [2, 7, 8, 18, 35, 36, 38, 45, 47, 52, 56, 57, 58, 60, 63, 64, 66, 72, 74, 80]
+
+    dataset = dataset_cl(opt, phase=opt.phase)
+    evaluate(model, dataset, opt)
+
+if opt.phase == "test":
+    model = models.OASIS_model(opt)
+    model = models.put_on_multi_gpus(opt, model)
+    model.eval()
+
+    dataset = dataset_cl(opt, phase=opt.phase)
+    
+    test_dataloader = DataLoader(dataset, batch_size=1, shuffle=False, drop_last=False)
+    
+    os.makedirs(os.path.join("results", opt.name, opt.phase + "_images"), exist_ok=True)
+    
+    for i, data_i in enumerate(test_dataloader):
+        print(i, "/", len(test_dataloader), end="\r")
+        image, label, human_parsing = models.preprocess_input(opt, data_i)
+        # label["cloth_seg"] = model.module.edit_cloth_seg(image["C_t"], label["body_seg"], label["cloth_seg"])
+        agnostic = data_i["agnostic"] if opt.bpgm_id.find("old") >= 0 else None
+        
+        if opt.no_seg:
+            image["I_m"] = image["I"]
+        
+        pred = model(image, label, "generate", None, agnostic=agnostic).detach().cpu().squeeze().permute(1, 2, 0).numpy()
+        # print(pred.shape)
+        pred = (pred + 1) / 2
+        pred = (pred * 255).astype(np.uint8)
+        pred = cv2.cvtColor(pred, cv2.COLOR_RGB2BGR)
+        # pred = cv2.resize(pred, (data_i['original_size'][1], data_i['original_size'][0]), interpolation=cv2.INTER_LINEAR)
+        pred = cv2.resize(pred, opt.img_size[::-1], interpolation=cv2.INTER_AREA)
+        
+        if opt.dataset == "mpv":
+            filename = data_i['name'][0].split("/")[-1].replace(".jpg", ".png")
+        elif opt.dataset == "viton":
+            filename = data_i['name'][0].split("/")[-1]
+        elif opt.dataset == "vitonHD":
+            filename = data_i['name'][0].split("/")[-1]
+        cv2.imwrite(os.path.join("results", opt.name, opt.phase + "_images", filename), pred)
+        cv2.imwrite(os.path.join("results", opt.name, opt.phase + "_images", filename, "_origin"), tens_to_im(image["I"][0]))
+        cv2.imwrite(os.path.join("results", opt.name, opt.phase + "_images", filename, "_cloth"), tens_to_im(image["C_t"][0]))
+        im = tens_to_lab(label["densepose_seg"][0], opt.semantic_nc[2] + 1)
+        cv2.imwrite(os.path.join("results", opt.name, opt.phase + "_images", filename, "_densepose"), im)
+
+
+
+
